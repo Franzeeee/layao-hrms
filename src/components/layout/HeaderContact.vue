95a9ead86f4b3a54797cdf71e66ca4ce220fde4b
Franze <diazfranzpeter@gmail.com>
2025-08-29 11:43:32 +0800

    fix: Update contact information in HeaderContact component

--- src/components/layout/HeaderContact.vue
+++ src/components/layout/HeaderContact.vue
@@ -1,24 +1,27 @@
 <template>
   <div class="hidden gap-10 md:flex">
+    <!-- Phone -->
     <div class="flex gap-2 text-sm">
       <div class="w-10 h-10 flex items-start justify-center">
-        <i class="fa-solid fa-user text-lg text-primary-color-1"></i>
+        <i class="fa-solid fa-phone text-lg text-primary-color-1"></i>
       </div>
       <div class="w-full flex-col h-full min-w-34 text-gray-500">
         <p>Have any question?</p>
-        <p>Free<span class="font-bold">: +12 365 5233</span></p>
+        <p>Call us<span class="font-bold">: +63 917 133 0373</span></p>
       </div>
     </div>
 
+    <!-- Email -->
     <div class="flex gap-2 text-sm">
       <div class="w-10 h-10 flex items-start justify-center">
         <i class="fa-solid fa-envelope text-lg text-primary-color-1"></i>
       </div>
       <div class="w-full flex-col h-full min-w-34 text-gray-500">
         <p>Have any question?</p>
-        <p>Free<span class="font-bold">: +12 365 5233</span></p>
+        <p>Email<span class="font-bold">: layaobeachresort@gmail.com</span></p>
       </div>
     </div>
   </div>
 </template>
+
 <script setup lang="ts"></script>
